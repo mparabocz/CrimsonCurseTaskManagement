13c51b9bf38dbbfe1be1b3e901d90fb7aadeafc4
Marcelo Parabocz <macerno@gmail.com>
2026-03-27 14:50:02 -0300

    feat: random user selector

---
+++ src/app/dummy-users.ts
@@ -0,0 +1,52 @@
+export const DUMMY_USERS = [
+  {
+    id: 'u1',
+    name: 'Anoux Abdalla',
+    avatar: 'user-1.png',
+  },
+  {
+    id: 'u2',
+    name: 'Barram Rambo',
+    avatar: 'user-2.png',
+  },
+  {
+    id: 'u3',
+    name: 'Capitão Elizandro',
+    avatar: 'user-3.png',
+  },
+  {
+    id: 'u4',
+    name: 'Drogan Alho Negro',
+    avatar: 'user-4.png',
+  },
+  {
+    id: 'u5',
+    name: 'Leonara Gallaur',
+    avatar: 'user-5.png',
+  },
+  {
+    id: 'u6',
+    name: 'Maudriara Beylana',
+    avatar: 'user-6.png',
+  },
+  {
+    id: 'u7',
+    name: 'Imediato Jaxon',
+    avatar: 'user-7.png',
+  },
+  {
+    id: 'u8',
+    name: 'Olivia Sylfar',
+    avatar: 'user-8.png',
+  },
+  {
+    id: 'u9',
+    name: ' Sabrina Weshenfelder',
+    avatar: 'user-9.png',
+  },
+  {
+    id: 'u10',
+    name: ' Borborig Tamzaren',
+    avatar: 'user-10.png',
+  },
+];
