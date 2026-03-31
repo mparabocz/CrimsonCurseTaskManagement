214f9f59db36396749f2225e46e54d45e6292c72
Marcelo Parabocz <macerno@gmail.com>
2026-03-31 15:14:49 -0300

    refactor: move logic from component to service

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,6 +1,7 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { type NewTaskData } from '../task/task.model';
+import { TasksService } from '../tasks.service';
 
 @Component({
   selector: 'app-new-task',
@@ -10,22 +11,28 @@ import { type NewTaskData } from '../task/task.model';
   styleUrl: './new-task.component.css',
 })
 export class NewTaskComponent {
-  @Output() cancel = new EventEmitter<void>();
-  @Output() add = new EventEmitter<NewTaskData>();
+  @Input({ required: true }) userId!: string;
+  @Output() close = new EventEmitter<void>();
 
   newTaskTitle = '';
   newTaskSummary = '';
   newTaskDate = '';
 
+  private tasksService = inject(TasksService);
+
   onCancel() {
-    this.cancel.emit();
+    this.close.emit();
   }
 
   onSubmit() {
-    this.add.emit({
-      title: this.newTaskTitle,
-      summary: this.newTaskSummary,
-      date: this.newTaskDate,
-    });
+    this.tasksService.addTask(
+      {
+        title: this.newTaskTitle,
+        summary: this.newTaskSummary,
+        date: this.newTaskDate,
+      },
+      this.userId,
+    );
+    this.close.emit();
   }
 }
